87a21a15559b7325c89ddfd0fcbfa172fe47c427
MarcosCorona <91139292+MarcosCorona@users.noreply.github.com>
2022-03-13 17:12:39 +0100

    asd

--- tiendas/models/models.py
+++ tiendas/models/models.py
@@ -27,7 +27,7 @@ class trabajador(models.Model):
     edad = fields.Integer('Edad', compute='_getEdad')
     #relaccion
     tienda_id = fields.Many2one('tiendas.tienda', string='Tienda')
-    camion_id = fields.One2many('rutas.camion','trabajador_id')
+    camion_id = fields.One2many('rutas.camion','trabajador_id2')
     #validacion
 
     @api.constrains('dniTrabajador')
